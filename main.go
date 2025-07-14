c13bbe5ead1b452aedc13292eb9ba156b45a5ca9
Zeeshan <zeeshanalivr46@gmail.com>
2025-07-15 00:21:27 +0530

    fix: unused test

--- main.go
+++ main.go
@@ -12,12 +12,4 @@ func main() {
 
 	t1.ViewTorrent()
 	t1.StartDownload()
-
-	// t2, err := torrent.NewTorrentFromFile("./test.torrent")
-	// if err != nil {
-	// 	panic(err)
-	// }
-
-	// t2.ViewTorrent()
-	// t2.StartDownload()
 }
