ac3f6fd63edf9afee14c0dc586f312d2a5a5a1ef
Zeeshan <zeeshanalivr46@gmail.com>
2025-07-27 19:02:12 +0530

    feat: send choke, unchoke messages

--- peers/peer.go
+++ peers/peer.go
@@ -33,13 +33,14 @@ func NewPeerManager(peers []Peer, infoHash, peerId []byte) *PeerManager {
 func (pm *PeerManager) HandlePeers() {
 	hs, err := handshake.NewHandshake(pm.infoHash, pm.peerId)
 	if err != nil {
-		panic(err)
+		fmt.Println("Error creating handshake:", err)
+		return
 	}
 
 	for i := range pm.peers {
 		p := &pm.peers[i]
 
-		conn, err := p.ConnectToPeer()
+		conn, err := p.connect()
 		if err != nil || conn == nil {
 			continue
 		}
@@ -55,30 +56,81 @@ func (pm *PeerManager) HandlePeers() {
 			continue // Ignore peers that do not match the handshake
 		}
 
-		bf, err := messages.ReceiveBitField(conn)
-		if err == nil {
+		p.conn = conn // Reuse the connection for further communication
+
+		bf, err := p.receiveBitField()
+		if err != nil {
+			fmt.Println(err)
+		} else {
 			p.Bitfield = bf
 		}
 
-		p.conn = conn
-
-		pm.connectedPeers = append(pm.connectedPeers, p)
+		// pm.connectedPeers = append(pm.connectedPeers, p)
+		p.sendUnchoke()
+		p.sendInterested()
 		p.exchangeMessages()
 	}
+}
+
+func (p *Peer) receiveBitField() ([]byte, error) {
+	msg, err := messages.Receive(p.conn)
+	if err != nil {
+		return nil, err
+	}
+
+	if msg.ID != messages.MsgBitfield {
+		return nil, fmt.Errorf("expected bitfield message (ID 5), but got ID %d", msg.ID)
+	}
+
+	if len(msg.Payload) == 0 {
+		return nil, fmt.Errorf("received empty bitfield payload from peer %s", p.IP.String())
+	}
+
+	fmt.Printf("\nReceived Bitfield: %x\n\n", msg.Payload)
+
+	return msg.Payload, nil
+}
+
+func (p *Peer) sendUnchoke() error {
+	msg := messages.Message{ID: messages.MsgUnchoke}
+	_, err := p.conn.Write(msg.Serialize())
+
+	fmt.Println("Sent `unchoke` message to peer:", p.IP.String())
+	return err
+}
+
+func (p *Peer) sendInterested() error {
+	msg := messages.Message{ID: messages.MsgInterested}
+	_, err := p.conn.Write(msg.Serialize())
+
+	fmt.Println("Sent `ineterested` message to peer:", p.IP.String())
+	return err
 }
 
 func (p *Peer) exchangeMessages() {
 	fmt.Println("\n\nStarting communication with peer: ", p.IP.String())
 
-	messages.ReceivePeerMessage(p.conn)
-	messages.ReceivePeerMessage(p.conn)
-	p.conn.Close()
+	for {
+		// messages.Send(p.conn)
+		msg, err := messages.Receive(p.conn)
+		if err != nil {
+			fmt.Println("Error receiving message from peer:", err)
+			p.conn.Close()
+			return
+		}
+
+		if msg.ID == messages.MsgCancel {
+			fmt.Println("Received cancel message from peer:", p.IP.String())
+			p.conn.Close()
+			return
+		}
+	}
 }
 
 // COnnect to the associated peer using its IP and Port.
 // Returns a net.Conn if successful, or an error if it fails.
 // Returned `net.Conn` MUST be closed later by the caller.
-func (p *Peer) ConnectToPeer() (net.Conn, error) {
+func (p *Peer) connect() (net.Conn, error) {
 	addr := net.JoinHostPort(p.IP.String(), strconv.Itoa(p.Port))
 	timeout := time.Second * 5
 	return net.DialTimeout("tcp", addr, timeout)
